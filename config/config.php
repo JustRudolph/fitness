44dbda2da243a04a71a89c8f81dd75c61d810d68
Flicks <144615488+Flicks66@users.noreply.github.com>
2025-02-20 11:53:55 +0100

    Initial commit with project files

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * De inloggegevens van de gebruiker van de database
+ */
+
+// Naam van de mysql-server waar de service mysql opstaat
+$dbHost = 'Localhost';
+
+// Naam van de database
+$dbName = 'logindb';
+
+// Naam van de gebruiker die de queries uitvoerd
+$dbUser = 'root';
+
+// Wachtwoord van gebruiker rra-2408b
+$dbPass = '';
